94c9db31c6b5840bd8f36be14c1c229cf1df6568
Nilufar Adat <51162107+Nil19@users.noreply.github.com>
2024-07-16 21:42:09 +0100

    Revert "Update ContentView.swift"

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,18 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
+        VStack {
+            Text("Hello SwiftUI!")
                 .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
+            Text("Joshua Tree National Park")
         }
-        .padding()
+            
+            
+            
     }
 }
 
@@ -29,4 +25,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
