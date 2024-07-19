97efa1fe5557e638a3d274cc92bd3193339bb0b4
Nil19 <nilufar_adat@hotmail.com>
2024-07-19 16:20:19 +0100

    Building lists and navigation

---
+++ LandmarkList.swift
@@ -0,0 +1,8 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Nil on 19/11/2024.
+//
+
+import Foundation
---
+++ Landmarks/Resources/Landmark.swift
@@ -0,0 +1,8 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Nil on 28/08/2024.
+//
+
+import Foundation
